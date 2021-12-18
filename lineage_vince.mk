b53d8c6766c48eb0a73e9e0d9bbaed3e506f7ddf
Aditya OP <95341296+Aknx77@users.noreply.github.com>
2021-12-18 19:17:23 +0530

    Update and rename arrow_vince.mk to lineage_vince.mk

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -28,11 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := arrow_vince
+PRODUCT_NAME := lineage_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
